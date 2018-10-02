13992f43d5dd87e1da126e96b0a5c8b5459fb2e9
Roan Mertens <roanmertens91@gmail.com>
2018-10-02 08:24:02 +0100

    removed redundant comments

--- vinterior/lib/checkout.rb
+++ vinterior/lib/checkout.rb
@@ -1,17 +1,7 @@
-# write test
-# write checkout
-
 # As user I can:
 # Make a new checkout with the selected promotional_rules.
 # Add items to my checkout.
 # Show the total price. (with the applied promotional_rules)
-
-# checkout class
-
-# checkout instance
-# Calculate the total price.
-# Add items in any possible order.
-# Add promotional_rules where a new checkout instance is made.
 
 require 'product'
 
