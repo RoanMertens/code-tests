94901e93cf3fb618fb39a446894ba961d8aed733
Roan Mertens <roanmertens91@gmail.com>
2018-11-14 15:20:35 +0000

    made a working version of hangman

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,23 +1,69 @@
 # The hangman game
 class Hangman
-  attr_accessor :mistakes, :word, :frame
+  attr_reader :mistakes, :frame, :guesses, :hidden, :end
   def initialize
     @mistakes = []
     @words = %w[kiss dancing zen forward]
     @word = pick_word
+    @hidden = []
     @frame = ['', '', '', '', '', '', '', '', '', '', '']
+    @guesses = 0
+    @end = false
+  end
+
+  def end?
+    check_mistakes
+    @end = true if @mistakes.length > 9 || @word == @hidden
+  end
+
+  def check_letter(letter)
+    if @word.include?(letter)
+      @word.each_with_index do |l, index|
+        @hidden[index] = letter if l == letter
+      end
+    elsif @mistakes.include?(letter)
+      puts "You alreaddy guessed the letter #{letter}"
+    else
+      @mistakes.push(letter)
+    end
+  end
+
+  def hide_word
+    @word.length.times { @hidden << '-' }
+  end
+
+  def word_length
+    @word.length
+  end
+
+  def show_word
+    @hidden.join(' ')
   end
 
   def pick_word
-    @words.sample
+    @word = @words.sample.split('')
   end
 
-  def play_word
+  def full_frame
+    ['___________',
+     '|/    |',
+     '|     |',
+     '|     O',
+     '|    /|\ ',
+     '|   / | \ ',
+     '|    / \ ',
+     '|   /   \ ',
+     '|',
+     '|',
+     '|\_________']
+  end
+
+  def check_mistakes
     case @mistakes.length
     when 0
       @frame = ['', '', '', '', '', '', '', '', '', '', '']
     when 1
-      @frame.last = '|\_________'
+      @frame[-1] = '|\_________'
     when 2
       @frame = ['', '|/', '|', '|', '|', '|', '|', '|', '|', '|',
                 '|\_________']
@@ -29,7 +75,7 @@ class Hangman
     when 5
       @frame[3] = ['|     O']
     when 6
-      @frame[4, 2] = ['|/    |', '|     |']
+      @frame[4, 2] = ['|     |', '|     |']
     when 7
       @frame[4, 2] = ['|    /|', '|   / |']
     when 8
--- hangman/interface.rb
+++ hangman/interface.rb
@@ -1,27 +1,43 @@
 # hangman interface
 require_relative 'hangman'
-
-puts 'Hangman'
-puts
-puts ''
 playing = 'y'
-outcome = 'lose'
-guesses = 0
 
 until playing.casecmp('n').zero?
+  puts `clear`
   hangman = Hangman.new
-  word = hangman.word
-  puts '- ' * word.length
-  puts "The word has #{word.length} letters."
-  puts "You have #{guesses} right guesses."
+  hangman.hide_word
 
-  puts "You #{outcome}!"
-  puts 'Want to play again? (y/n)'
+  puts 'Hangman'
+  puts hangman.full_frame
+  puts 'Yeeehaw!'
+  sleep(1)
+
+  until hangman.end == true
+    puts `clear`
+    puts hangman.frame
+    hangman.hidden
+    word = hangman.show_word
+    puts word
+    puts "The word has #{hangman.word_length} letters."
+    puts 'What is your guess? (Only one letter please!)'
+    letter = gets.chomp
+
+    hangman.check_letter(letter)
+    hangman.check_mistakes
+    puts hangman.end?
+    puts hangman.mistakes.length
+  end
+
+  puts hangman.frame
+  puts `clear`
+  puts "You ended!"
+  puts
+  # input a word
+  # show lines
+  # show length word
+  # make guess
+  # check guess
+
+  puts 'Do you want to play again? (y/n)'
   playing = gets.chomp
 end
-
-counter = 0
-
-puts 'Which letter do you guess?'
-
-letter = gets.chomp
